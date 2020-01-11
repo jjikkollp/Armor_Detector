b1500ac40226c07df109756388c39c18526b36cc
jjikkollp <jjikkollp@163.com>
2020-01-11 15:06:44 +0800

    在主函数添加了读取帧的代码

--- main.cpp
+++ main.cpp
@@ -2,14 +2,22 @@
 
 cv::Mat frame; //要处理的每一帧图像。
 
+std::string FILEPATH="/pos/"; //存储装甲板图像的文件相对路径
+cv::VideoCapture cam(0); //laptop摄像头初始化
+int Current_Frame=1; //读取文件时的计数器。
 
-
+//读取每一帧
 bool Load_Frame(int type){
     if(type == 0){
-
+        if(Current_Frame > 667) return false; // 文件读取完了
+        std::string file = FILEPATH+"a"+std::to_string(Current_Frame)+".jpg";
+        frame = cv::imread(file);
+        ++Current_Frame;
+        return true;
     }
     else if(type == 1){
-
+        cam >> frame;
+        return true;
     }else{
         return false;// TODO:从云台摄像头读取。
     }
@@ -18,7 +26,8 @@ bool Load_Frame(int type){
 int main(int argc,char *argv[]){
     initopts(argc,argv);
     while(Load_Frame(CAMERA_TYPE)){
-
+        
     }
+    fprintf(stderr,"The Program ended successfully!");
     return 0;
 }
--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <opencv2/opencv.hpp>
+#include <cctype>
 #include "options.h"
 
 #endif
