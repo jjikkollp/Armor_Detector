971dc06ed83dd0d04a275ab71b244441896c9188
jjikkollp <jjikkollp@163.com>
2020-01-11 15:46:38 +0800

    装甲板的定义添加数字

--- types.h
+++ types.h
@@ -19,7 +19,7 @@ class Armor_box{
 public:
     cv::Rect2d ABRect; //装甲板矩形
     std::vector<LightBlob> LBS; //装甲板对应的灯条们
-
+    int id; //装甲板的数字
     //构造函数
     Armor_box(cv::Rect2d Rect,std::vector<LightBlob> LB):ABRect(Rect),LBS(LB){}
     cv::Point2f Center(); //获取装甲板中心
