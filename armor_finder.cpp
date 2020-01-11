b4edf8f56847b4c5789e76cf24f0a9233828b693
jjikkollp <jjikkollp@163.com>
2020-01-11 15:57:49 +0800

    更新armorfinder类

--- armor_finder.cpp
+++ armor_finder.cpp
@@ -1,2 +1,9 @@
 #include "armor_finder.h"
 
+void Armor_Detector::work(cv::Mat &frame){
+    std::vector<LightBlob> LiBs; //存储图中所有可能的灯条
+    std::vector<Armor_box> ArBs; //存储途中所有可能的装甲板
+
+    //在图上寻找灯条
+    
+}
--- armor_finder.h
+++ armor_finder.h
@@ -3,10 +3,13 @@
 
 #include<opencv2/opencv.hpp>
 #include "types.h"
+#include "options.h"
+#include "lb_finder.h"
 
 //装甲板detector类
 class Armor_Detector{
 public:
+    //装甲板识别主函数
     void work(cv::Mat &frame);
 };
 
